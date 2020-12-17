10f403c89366093b1a4a97473f7d7038404f2aa5
jtjun <tae7130@snu.ac.kr>
2020-12-17 23:33:27 +0900

    feat: css styling

--- PyQt5X/XWindows/qtxwindow.py
+++ PyQt5X/XWindows/qtxwindow.py
@@ -4,6 +4,7 @@ Inherits QtXWindow class and
 implements set_ui method
 using self.gen_{WIDGET_NAME}
 """
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5X.errors import *
@@ -22,9 +23,10 @@ class QtXWindow(QMainWindow):
         resizable: bool, fixed size or not, initially resizable (True)
     """
     app = QApplication([])
-    def __init__(self, title, icon_path=None, width=None, height=None, resizable=True):
+    def __init__(self, title, icon_path=None, width=None, height=None, resizable=True, style_path=None):
         super().__init__()
         self.setWindowTitle(title)
+        self.setAutoFillBackground(True)
         if icon_path:
             self.setWindowIcon(QIcon(icon_path))
         # size
@@ -35,4 +37,21 @@ class QtXWindow(QMainWindow):
             width, height) # center of monitor
         if not resizable:
             self.setFixedSize(width, height)
+        # style
+        if style_path:
+            self.set_csstyle(css_file=style_path)
 
+    def set_csstyle(self, css_file=None, style_dict=None):
+        """set style with css"""
+        if not (css_file or style_dict):
+            raise ParmEmptyError('css_file or style_dict is required') 
+        if css_file:
+            with open(css_file, 'r', encoding='utf-8') as style:
+                self.setStyleSheet(style)
+        if style_dict:
+            # TODO: parse dict and apply style
+            pass
+
+    def set_translucent_background(self):
+        """Set the background transparent"""
+        self.setAttribute(Qt.WA_TranslucentBackground)
