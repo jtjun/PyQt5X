088fcecdf30fcc9bf068896917401c4c1bc675e2
jtjun <tae7130@snu.ac.kr>
2020-12-17 21:59:08 +0900

    test: PyQt5X version test

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""Test the PyQt5X
+initialize test enviroment
+"""
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
---
+++ test/__main__.py
@@ -0,0 +1,6 @@
+"""Execute PyQt5X tests
+"""
+import __init__
+import PyQt5X
+
+print(PyQt5X.ver)
