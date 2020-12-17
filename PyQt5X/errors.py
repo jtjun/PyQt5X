8c569516ab4e72fe8025b0484583a9f86fa62af7
jtjun <tae7130@snu.ac.kr>
2020-12-17 23:17:01 +0900

    feat: define parameter errors

--- PyQt5X/errors.py
+++ PyQt5X/errors.py
@@ -3,3 +3,12 @@
 
 class Error(Exception):
     """Base Exception fo all exceptions raised by PyQt5X"""
+
+class ParmError(Error):
+    """Parameter Error"""
+
+class ParmEmptyError(ParmError):
+    """Parameter did not passed"""
+
+class ParmTypeError(ParmError):
+    """Invalid type of parameter has passed"""
