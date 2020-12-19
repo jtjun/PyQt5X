d5ab8073c6b4b54eb686f5746b63f96a44d2000a
jtjun <tae7130@snu.ac.kr>
2020-12-19 12:29:06 +0900

    refactor: rename test dir to xtest

--- xtest/XWindows/qtxwindow_test.py
+++ xtest/XWindows/qtxwindow_test.py
@@ -1,4 +1,5 @@
 """PyQt5.XWindow.QtXWindow Test"""
+from xtest import DATA_PATH
 from PyQt5X.XWindows import QtXWindow
 
 def basic_init_test():
@@ -10,7 +11,14 @@ def basic_init_test():
     # resizable
     QtXWindow('test-fixed', width=150, height=150, resizable=False).show()
     # css-style
-    QtXWindow('test-csstyle', style_path='test\\data\\basic_init.css').show()
+    QtXWindow('test-csstyle', style_path=DATA_PATH + 'basic_init.css').show()
     # with
     with QtXWindow('test-with') as window:
         print(id(window))
+
+def font_test():
+    """QtXWindow font tests"""
+    window = QtXWindow('test')
+    window.regist_font_family(DATA_PATH + 'Noto_Sans\\NotoSans-Regular.ttf')
+    font = window.gen_font('NotoSans', 13, bold=True)
+    window.show()
--- xtest/__main__.py
+++ xtest/__main__.py
@@ -2,9 +2,10 @@
 """
 import __init__
 import PyQt5X
-from XWindows.qtxwindow_test import basic_init_test
+from XWindows.qtxwindow_test import basic_init_test, font_test
 
 
 if __name__ == '__main__':
     print(PyQt5X.ver)
-    basic_init_test()
+    #basic_init_test()
+    font_test()
